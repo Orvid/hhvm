fedcb982b3ea1d446ad70f2ea39408cac4e8507f
Orvid King <blah38621@gmail.com>
2015-08-15 22:07:13 -0500

    Implement a set of workarounds for limitations of MSVC
    These workarounds are ones that will hopefully be able to be removed in future versions of MSVC, but are needed to compile with the current release of MSVC 2015.

--- hphp/runtime/vm/jit/native-calls.cpp
+++ hphp/runtime/vm/jit/native-calls.cpp
@@ -66,6 +66,10 @@ using ResCmpFn = bool (*)(const ResourceHdr*, const ResourceHdr*);
 
 //////////////////////////////////////////////////////////////////////
 
+#ifdef MSVC_REQUIRE_AUTO_TEMPLATED_OVERLOAD
+static auto Generator_Create_false = &Generator::Create<false>;
+#endif
+
 /*
  * The table passed to s_callMap's constructor describes helpers calls
  * used by translated code. Each row consists of the following values:
@@ -313,8 +317,13 @@ static CallMap s_callMap {
                            {{SSA, 0}, {SSA, 1}, {SSA, 2}}},
 
     /* Generator support helpers */
+#ifdef MSVC_REQUIRE_AUTO_TEMPLATED_OVERLOAD
+    {CreateCont,         Generator_Create_false, DSSA, SNone,
+                           {{SSA, 0}, {SSA, 1}, {SSA, 2}, {SSA, 3}}},
+#else
     {CreateCont,         &Generator::Create<false>, DSSA, SNone,
                            {{SSA, 0}, {SSA, 1}, {SSA, 2}, {SSA, 3}}},
+#endif
 
     /* Async function support helpers */
     {CreateAFWH,         &c_AsyncFunctionWaitHandle::Create<true>, DSSA, SNone,
